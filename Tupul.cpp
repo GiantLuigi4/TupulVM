fb845e01c709ab09de3a26adcdfd94453081b888
GiantLuigi4 <tjmferet@hotmail.com>
2022-02-12 22:48:00 -0500

    I failed with git again

--- Tupul.cpp
+++ Tupul.cpp
@@ -3,10 +3,16 @@
 
 #include "Tupul.h"
 #include "Utils.h"
+#include "FileSystems.h"
 
 using namespace std;
 
-int main() {
-	println(absolute("test.txt"));
+int main(int argc, char** args) {
+	println(getWorkingDir().substr(0, getWorkingDir().find_last_of("/")));
+	string test = "./hello.txt";
+	test = test.substr(test.find_first_of("/") + 1);
+	println(test);
+	println(absolutePath("test.txt"));
+	println(absolutePath("../test.txt"));
 	return 0;
 }
--- Utils.cpp
+++ Utils.cpp
@@ -3,46 +3,9 @@
 #include <cstdio>
 #include <string>
 
-// https://stackoverflow.com/questions/8666378/detect-windows-or-linux-in-c-c/33088568
-// cross platform support
-#if defined(_WIN32) || defined(WIN32)
-    // support for windows
-    #include <stdlib> // can I move this out of the ifdef? ig mac will decide that, now that I think about it
-    #define MAX_PATH _MAX_PATH // windows, why is there an _ before the variable now
-
-    // fullpath method
-    void fullpath(char* dst, const char* src) {
-        _fullpath(full, path.c_str(), _MAX_PATH);
-    }
-#elif __unix__
-    // support for linux
-    #include <unistd.h> // why do I need this..? do I need this..?
-    #include <stdlib.h> // I think I need this here, but idk!
-    #include <limits.h> // PATH_MAX
-    // #define _MAX_PATH pathconf("/", 4)
-    #define MAX_PATH PATH_MAX // unix, why is it PATH_MAX instead of MAX_PATH
-
-    // fullpath method
-    // https://stackoverflow.com/questions/2341808/how-to-get-the-absolute-path-for-a-given-relative-path-programmatically-in-linux
-    void fullpath(char* dst, const char* src) {
-        realpath(src, dst);
-    }
-#endif
-
 using namespace std;
 
 #define null nullptr
-
-string absolute(string path) {
-    char* full = new char[MAX_PATH];
-    fullpath(full, path.c_str());
-    if (full != null) {
-        string str = "";
-        for (int i = 0; i < MAX_PATH; i++) if (full[i] != 0) str += full[i]; else break;
-        return str;
-    }
-    return "?!`'|intendedcrashinvalidpath!?whydidInotuseapointer, lol";
-}
 
 // prints a c++ string
 void print(string str) {
